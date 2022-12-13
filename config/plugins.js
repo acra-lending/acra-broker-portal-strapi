b0db2e36e4831d70d08452ae514efca631378528
Chris Liu <chrisl@CSC.local>
2022-12-13 14:01:50 -0800

    Add ratelimit to user-permissions

--- config/plugins.js
+++ config/plugins.js
@@ -34,5 +34,13 @@ module.exports = ({ env }) => ({
           },
         ]
       }
-  }
+    },
+    'users-permissions': {
+      config: {
+        ratelimit: {
+          interval: 60000,
+          max: 10000
+        }
+      }
+    }
   });
--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.acralending.com',
+  // url: 'https://api.acralending.com',
   // url: 'https://2da9-107-194-134-60.ngrok.io',
-  // url: 'http://localhost:1337',
+  url: 'http://localhost:1337',
   app: {
     keys: env.array('APP_KEYS'),
   },
