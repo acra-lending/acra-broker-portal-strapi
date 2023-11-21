16886be3f3e7957b46675426939eaef7ecd6239c
parimalap-git <parimala.p@stellaripl.com>
2023-11-21 14:37:30 +0530

    Stellar 28

---
+++ src/api/sellers-guide/controllers/sellers-guide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sellers-guide controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sellers-guide.sellers-guide');
---
+++ src/api/sellers-guide/routes/sellers-guide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sellers-guide router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sellers-guide.sellers-guide');
