dfb4e3cace945b6f3b1ba4e61fae8a210ddceab9
parimalap-git <parimala.p@stellaripl.com>
2024-03-28 16:57:21 +0530

    Stellar144

---
+++ src/api/program-matrix/controllers/program-matrix.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * program-matrix controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::program-matrix.program-matrix');
---
+++ src/api/program-matrix/routes/program-matrix.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * program-matrix router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::program-matrix.program-matrix');
